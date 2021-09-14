c498f7a276d748baacad0f368fa117014f3984ce
jasminappleby <jasminapplebyholdham@hotmail.com>
2021-09-14 15:48:20 +0100

    form component

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,1 +1,25 @@
 import React from 'react';
+
+function Form({ getResult }) {
+
+    const [ location, setLocation ] = useState("")
+
+    const handleSubmit = e => {
+        getResult(location);
+    }
+
+    const updateInput = e => {
+        const input = e.target.value 
+        setLocation(input)
+    }
+
+
+    return (
+        <form onSubmit={handleSubmit} role="form">
+            <input type="text" aria-label="Location" onChange={e => updateInput(e)} />
+            <input type="submit" value="Search" />
+        </form>
+    );
+};
+
+export default Form;
