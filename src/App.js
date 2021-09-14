5737cfb120908714819b65ab199cc05b0a903543
jasminappleby <jasminapplebyholdham@hotmail.com>
2021-09-14 16:45:28 +0100

    made the api work

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import { Form } from './components';
 
 export default function App() {
-    return <div>Hi.</div>
+    return (
+        <>
+            <Form />
+        </>
+    )
 }
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Form({  }) {
+function Form() {
 
-    const [ location, setLocation ] = useState("")
+    const [ user, setUser ] = useState("")
 
-    const handleSubmit = e => {
+    const handleSubmit = async e => {
         e.preventDefault()
+        const response = await fetch(`https://api.github.com/users/${user}/repos`)
+        const result = await response.json()
     }
 
     const updateInput = e => {
         const input = e.target.value 
-        setLocation(input)
+        setUser(input)
     }
 
     return (
         <form onSubmit={handleSubmit} role="form">
-            <input type="text" aria-label="Location" onChange={e => updateInput(e)} />
+            <input type="text" aria-label="Username" onChange={e => updateInput(e)}/>
             <input type="submit" value="Search" />
         </form>
     );
