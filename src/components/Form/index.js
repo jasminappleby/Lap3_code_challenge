2be308a0149c0c8ca11013da682bc3fe5e16005e
jasminappleby <jasminapplebyholdham@hotmail.com>
2021-09-14 17:15:26 +0100

    added a fetch requester

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -2,13 +2,18 @@ import React, { useState } from 'react';
 
 function Form() {
 
-    const [ user, setUser ] = useState("")
+    const [ user, setUser ] = useState()
+
+    const [fetched, setFetched] = useState(false)
 
     const handleSubmit = async e => {
         e.preventDefault()
         const response = await fetch(`https://api.github.com/users/${user}/repos`)
         const result = await response.json()
+        setFetched(true)
     }
+
+    // const renderResult = () => <Result result={result}/>
 
     const updateInput = e => {
         const input = e.target.value 
@@ -16,10 +21,13 @@ function Form() {
     }
 
     return (
-        <form onSubmit={handleSubmit} role="form">
-            <input type="text" aria-label="Username" onChange={e => updateInput(e)}/>
-            <input type="submit" value="Search" />
-        </form>
+        <div>
+            <form onSubmit={handleSubmit} role="form">
+                <input type="text" aria-label="Username" onChange={e => updateInput(e)}/>
+                <input type="submit" value="Search" />
+            </form>
+            { fetched ? <p>penguins</p> : <p>go away penguins</p> }   
+        </div>
     );
 };
 
